f9f92adb41dfe546906c8ee9bea8e9649af478c8
Adam Smaka <adamsmaka@gmail.com>
2021-01-26 12:51:06 +0100

    Take down Android only limitation from the write method.

--- packages/health/lib/src/health_factory.dart
+++ packages/health/lib/src/health_factory.dart
@@ -159,10 +159,6 @@ class HealthFactory {
     int value = 0,
     String name = "",
   }) async {
-    if (_platformType == PlatformType.IOS) {
-      print('Writing is not supported on iOS');
-      return;
-    }
     print("AWAITING PERMISSION");
     bool granted = await requestAuthorization([dataType]);
     print("PERMISSION: " + granted.toString());
